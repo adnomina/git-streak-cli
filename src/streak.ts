c50ff5d35a8a7b8c524c8f7867454a66e4b0f95d
Nicolas Herschke <nicolas.herschke@gmx.de>
2025-11-19 18:16:11 +0100

    feat: add login command that authenticates the user with GitHub OAuth

--- src/streak.ts
+++ src/streak.ts
@@ -1,19 +1,52 @@
-import { Command } from 'commander';
+import { Command } from "commander";
+import { createOAuthDeviceAuth } from "@octokit/auth-oauth-device";
+import { Octokit } from "@octokit/core";
 
 const program = new Command();
 
 program
-    .name('streak')
-    .description('A CLI tool for checking your GitHub streak')
-    .version('1.0.0');
+    .name("streak")
+    .description("A CLI tool for checking your GitHub streak")
+    .version("1.0.0");
 
-program.command('status')
-    .description('Show the current status of your GitHub streak')
-    .option('--json', 'machine-readable output')
-    .option('--tz', 'timezone identifier')
+program
+    .command("status")
+    .description("Show the current status of your GitHub streak")
+    .option("--json", "machine-readable output")
+    .option("--tz", "timezone identifier")
     .action(() => {
-        console.log('Hello, world!');
+        console.log("Hello, world!");
     });
 
+async function logInToGitHub() {
+    const auth = createOAuthDeviceAuth({
+        clientId: "Ov23li4rxHZ0fAdDEyh7",
+        scopes: ["read:user"],
+        async onVerification(verification) {
+            console.log("Open %s", verification.verification_uri);
+            console.log("Enter code: %s", verification.user_code);
+        },
+    });
 
-program.parse();
+    const { token } = await auth({
+        type: "oauth",
+    });
+
+    const octokit = new Octokit({
+        auth: token,
+    });
+
+    try {
+        const { data: userInfo } = await octokit.request("GET /user");
+        console.table(userInfo);
+    } catch (error) {
+        console.error("Error fetching user information with Octokit:", error);
+    }
+}
+
+program
+    .command("login")
+    .description("Authenticate with GitHub OAuth Device Authentication")
+    .action(logInToGitHub);
+
+await program.parseAsync();
