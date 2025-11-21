7ad040d74910b296ec0cad3786307e372e014a96
Nicolas Herschke <nicolas.herschke@gmx.de>
2025-11-21 18:05:26 +0100

    feat: use the GraphQL API instead of REST for better handling of contributions

--- src/subcommands/status.ts
+++ src/subcommands/status.ts
@@ -1,40 +1,27 @@
 import fs from "node:fs/promises";
-import { Octokit } from "@octokit/core";
+import { graphql } from "@octokit/graphql";
+import type { GraphQlQueryResponseData } from "@octokit/graphql/types";
 
 export default async function status() {
 	const token = await fs.readFile("token.txt", "utf8");
 
-	const octokit = new Octokit({
-		auth: token,
-	});
-
-	await octokit
-		.request("GET /users/{username}/events", {
-			username: "adnomina",
-		})
-		.then((response) => {
-			const events = response.data;
-			console.log(events);
-
-			let commits = 0;
-
-			events.forEach((event) => {
-				console.log(event);
-				/*
-                if (!(
-                    event.created_at?.startsWith(new Date().toISOString().split('T')[0])
-                    && event.type === "PushEvent"
-                )) {
-                    return;
+	const result: GraphQlQueryResponseData = await graphql(
+		`
+            {
+                user(login: "adnomina") {
+                    contributionsCollection {
+                        totalCommitContributions
+                        totalRepositoryContributions
+                        totalPullRequestContributions
+                    }
                 }
-                */
-
-				commits += 1;
-			});
-
-			console.log(`Congrats! You've committed today (${commits} commits)`);
-		})
-		.catch((reason) => {
-			console.log(reason);
-		});
+            }
+        `,
+		{
+			headers: {
+				authorization: `token ${token}`,
+			},
+		},
+	);
+	console.table(result.user.contributionsCollection);
 }
